4cbc16d5e7f1e1fdd1c8acd523afc8d8c4681b09
brendenjones12 <brendenjones4320@gmail.com>
2020-02-13 10:15:09 -0500

    forgot to commit for steps, commiting for the making the high medium and low strategy classes

---
+++ src/ifs/InsuranceCalculator.java
@@ -0,0 +1,24 @@
+package ifs;
+
+public class InsuranceCalculator {
+
+    private InsuranceStrategyVeryHigh strategy;
+
+
+    public double calculateInsurance(double income) {
+
+
+        if (income <= 10000) {
+            return income * 0.365;
+        } else if (income <= 30000) {
+            return (income - 10000) * 0.2 + 35600;
+        } else if (income <= 60000) {
+            return (income - 30000) * 0.1 + 76500;
+        } else {
+            strategy = new InsuranceStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
+        }
+
+
+    }
+}
---
+++ src/ifs/InsuranceStrategyHigh.java
@@ -0,0 +1,18 @@
+package ifs;
+
+public class InsuranceStrategyHigh extends InsuranceStrategy {
+    @Override
+    public int getConstant() {
+        return 0;
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public int getAdjustment() {
+        return 0;
+    }
+}
